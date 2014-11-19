e35e74efc8744406c767a7a280ba27355a9f90ec
高田周 <takadaamane@takadaamane-no-MacBook-Air.local>
2014-11-19 23:38:08 +0900

    TweetSample

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  before_action :authenticate_user!, only: :show
+  def index
+  end
+
+  def show
+  @user = User.find_by(:username => current_user[:username])
+  end
+end
